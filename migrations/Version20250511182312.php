04171eba167aca9b99a66168ffbec5c626d511d4
revoltekdaniel <github@revoltek.de>
2025-05-11 20:23:54 +0200

    Switch from PostgreSQL to MySQL, update service configuration, and adjust user table schema.

---
+++ migrations/Version20250511182312.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250511182312 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user DROP city, DROP country, DROP name
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user ADD city VARCHAR(255) NOT NULL, ADD country VARCHAR(255) NOT NULL, ADD name VARCHAR(255) NOT NULL
+        SQL);
+    }
+}
