a0719c4210a94ad90f675d95df0d8e163ca931d1
revoltekdaniel <github@revoltek.de>
2025-05-22 20:41:38 +0200

    style: add frontend styles and update layout templates for improved design

--- importmap.php
+++ importmap.php
@@ -16,6 +16,10 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'frontend' => [
+        'path' => './assets/frontend.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
